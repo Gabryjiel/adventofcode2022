5f71e3b541532d3ad38a592f9240846979937cd0
Gabryjiel <gabriel.kudyba@gmail.com>
2022-12-06 00:02:38 +0100

    day5 finished

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -1,7 +1,6 @@
-use std::borrow::BorrowMut;
-
 fn main() {
   task1(); 
+  task2();
 }
 
 fn task1() {
@@ -14,15 +13,19 @@ fn task1() {
   let mut crates_final:  Vec< Vec<char>> =  vec![];
 
   for _ in 1..no_of_lines {
-    crates_final.push( vec![]);
+    crates_final.push(vec![]);
   }
 
   data
     .split('\n')
     .map(|x| x.chars().enumerate())
-    .for_each(|x| x.filter(|(index, value)| value.is_alphabetic()).for_each(|(index, value)| crates_final[index].push(value)));
+    .for_each(|x| x.filter(|(_, value)| value.is_alphabetic()).for_each(|(index, value)| crates_final[index].push(value)));
   
-  let mut crates_parsed:  Vec<Vec<char>> = crates_final.iter().filter(|x| x.len() > 0).clone().collect::<Vec<_>>();
+  let crates_parsed = crates_final
+    .iter()
+    .filter(|x| x.len() > 0)
+    .map(|x| x.clone())
+    .collect::<Vec<_>>();
 
   let commands_parsed = commands
     .split('\n')
@@ -37,26 +40,86 @@ fn task1() {
         .unwrap_or(0))
       .filter(|x| x > &0)
       .collect::<Vec<_>>())
-    .map(|x| (x[0], x[1], x[2]))
+    .map(|x| (x[0], x[1] - 1, x[2] - 1))
     .collect::<Vec<_>>();
 
+  let mut state = crates_parsed.clone();
   for (amount, start, end) in commands_parsed {
-    let mut crates_to_move = crates_parsed[start].get(0..amount).clone().iter().rev().clone().collect::<Vec<_>>().to_vec();
+    let crates_to_move = state[start][0..amount].iter().collect::<String>().chars().collect::<Vec<_>>();
 
-    let mut start_stack = crates_parsed[start].clone().get(0..amount).unwrap().to_vec();
-    let mut end_stack = crates_parsed[end].clone().into_iter().chain(&crates_to_move.iter());
+    let start_stack = state[start].clone().get(amount..).unwrap().to_vec();
+    let mut end_stack = state[end].clone();
 
+    end_stack.reverse();
+    crates_to_move.iter().for_each(|x| end_stack.push(*x));
+    end_stack.reverse();
+
+    state[start] = start_stack;
+    state[end] = end_stack;
+  }      
+
+  let word = state.iter().map(|x| x[0]).collect::<String>();
+
+  println!("{:?}", word);
+}
+
+fn task2() {
+  let input = std::fs::read_to_string("./src/bin/day5/input.txt")
+    .expect("Unable to read the file");
+
+  let (data, commands) = input.split_once("\n\n").unwrap();
+
+  let no_of_lines = 36;
+  let mut crates_final:  Vec< Vec<char>> =  vec![];
+
+  for _ in 1..no_of_lines {
+    crates_final.push(vec![]);
   }
 
-  // let res = commands_parsed
-  //   .iter()
-  //   .for_each(|(amount, start, end)| {
-  //     let mut crates_to_move = crates_parsed[*start][0..*amount].iter().rev().collect::<Vec<&char>>().as_mut().collect::<Vec<char>>();
+  data
+    .split('\n')
+    .map(|x| x.chars().enumerate())
+    .for_each(|x| x.filter(|(_, value)| value.is_alphabetic()).for_each(|(index, value)| crates_final[index].push(value)));
+  
+  let crates_parsed = crates_final
+    .iter()
+    .filter(|x| x.len() > 0)
+    .map(|x| x.clone())
+    .collect::<Vec<_>>();
 
-  //     // crates_parsed[*start] = &crates_parsed[*start].get(*amount..);
-  //     crates_parsed[*end] = crates_parsed[*end].append(crates_to_move);
-  //   });
-      
+  let commands_parsed = commands
+    .split('\n')
+    .map(|x| String::from_iter(x
+      .chars()
+      .filter(|x| !x.is_alphabetic())))
+    .map(|x| String::from_iter(x.get(1..)))
+    .map(|x| x
+      .split(' ')
+      .map(|x| x
+        .parse::<usize>()
+        .unwrap_or(0))
+      .filter(|x| x > &0)
+      .collect::<Vec<_>>())
+    .map(|x| (x[0], x[1] - 1, x[2] - 1))
+    .collect::<Vec<_>>();
 
-  println!("{:?}", commands_parsed);
+  let mut state = crates_parsed.clone();
+  for (amount, start, end) in commands_parsed {
+    let mut crates_to_move = state[start][0..amount].iter().collect::<String>().chars().collect::<Vec<_>>();
+
+    let start_stack = state[start].clone().get(amount..).unwrap().to_vec();
+    let mut end_stack = state[end].clone();
+
+    crates_to_move.reverse();
+    end_stack.reverse();
+    end_stack.append(&mut crates_to_move);
+    end_stack.reverse();
+
+    state[start] = start_stack;
+    state[end] = end_stack;
+  }      
+
+  let word = state.iter().map(|x| x[0]).collect::<String>();
+
+  println!("{:?}", word);
 }
