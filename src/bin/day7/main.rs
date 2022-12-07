898eb1e9433b7eadb98f7db25b6fc81fa186c252
Gabryjiel <gabriel.kudyba@gmail.com>
2022-12-07 12:22:29 +0100

    works for day7

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use orange_trees::{Node, Tree};
 
 fn main() {
@@ -8,37 +10,73 @@ fn task1() {
   let input = std::fs::read_to_string("./src/bin/day7/example.txt")
     .expect("Unable to read the file");
 
-  let mut tree: Tree<&str, u32> = Tree::new(
+  let tree: Tree<&str, u32> = Tree::new(
     Node::new("/", 0)
   );
 
-  input
+  let mut defa = String::from("/");
+
+  let dirs = vec!["/"];
+
+  let (new_tree, _) = input
     .split('\n')
-    .fold(tree.root_mut(), |acc, cur| {
+    .fold((tree, &mut defa), |acc, cur| {
+      let (mut tree, position) = acc;
+
       if cur.contains("$ cd ..") {
-        let id = acc.parent(acc.id()).expect("No parent").id().clone();
-        return acc.query_mut(&id).expect("err");
+        let address = position.split('/').collect::<Vec<_>>();
+        let ad = &address[0..address.len()].join("/");
+
+        position.clear();
+        position.push_str(ad);
+
+        return (tree, position);
       } else if cur.contains("$ cd") {
         let id = cur.split_whitespace().last().expect("No last element");
-        return acc.query_mut(&id).expect("Invalid child");
+
+        position.push('/');
+        position.push_str(id);
+
+        return (tree, position);
       } else if cur.contains("$ ls") {
-        return acc;
+        return (tree, position);
       } else if cur.contains("dir") {
         let name = cur.split(' ').last().expect("No name for dir");
+        let me = position.clone();
         
-        acc.add_child(Node::new(name, 0));
+        let current_node = tree.root_mut().query_mut(&(me.as_str()))
+          .expect("Unable to find current node");
         
-        return acc;
+        current_node.add_child(Node::new(name, 0));
+        drop(current_node);
+        
+        return (tree, position);
       } else {
-        println!("{}", cur);
         let (size, name) = cur.split_once(' ').expect("Invalid split");
         let parsed_size = size.parse::<u32>().expect("Error parsing");
 
-        acc.add_child(Node::new(name, parsed_size));
+        let current_node = tree.root_mut().query_mut(&(position.clone().as_str()))
+          .expect("Unable to find current node");
+
+        current_node.add_child(Node::new(name, parsed_size));
         
-        return acc;
+        return (tree, position);
       }
     });
 
-    println!("{:?}", tree);
+    let dirs_with_size = dirs.iter().map(|x| calc_node_value(new_tree.root().query(x).expect("No id")))
+    .filter(|size| size < &100000)
+    .sum::<u32>();
+
+    println!("{:?}", dirs_with_size);
+}
+
+fn calc_node_value(node: &Node<&str, u32>) -> u32 {
+  let value = *node.value();
+
+  if value != 0 {
+    return value;
+  } else {
+    return value + node.children().iter().map(|x| calc_node_value(x)).sum::<u32>();
+  }
 }
